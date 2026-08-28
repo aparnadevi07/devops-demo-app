bd6422d1e587b49fe78c42f9f0e4d75b0cf6f821
Aparna Devi S <163376860+aparnadevi07@users.noreply.github.com>
2026-08-28 11:32:37 +0530

    added teams

--- app/src/test/java/com/sap/training/ApplicationTests.java
+++ app/src/test/java/com/sap/training/ApplicationTests.java
@@ -24,7 +24,7 @@ class ApplicationTests {
             "http://localhost:" + port + "/", String.class);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).isEqualTo("Hello from Circulam Team. This is a devops demo");
+        assertThat(response.getBody()).isEqualTo("Hello from Circulam  This is a devops demo");
     }
 
     @Test
